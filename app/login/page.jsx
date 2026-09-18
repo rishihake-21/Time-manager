7e0b14968cf7b37da90cffd783731e6eba4bf6c3
rishihake-21 <rishikesh.hake1008@gmail.com>
2026-09-18 16:18:54 +0530

    new changes

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -34,6 +34,9 @@ export default function LoginPage() {
         const { data, error } = await supabase.auth.signUp({
           email,
           password,
+          options: {
+            emailRedirectTo: `${window.location.origin}/login`,
+          },
         });
 
         if (error) {
